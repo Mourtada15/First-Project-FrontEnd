014af5303b31035b11dc810f401aeb4216f93a91
Hassan Mourtada <hassanmourtada15>
2023-11-01 19:32:28 +0200

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import Navbar from "./components/navbar";
 import "./App.css"
+import Lebnene_Ele from "./Pages/Lebnene_Ele/Lebnene_Ele";
+
 const App = () =>{
     return(
+        <>
         <Navbar/>
+        <Lebnene_Ele />
+        </>
     );
 }
 
